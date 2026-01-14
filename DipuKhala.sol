0b97feccb0502e53b69e976b1c6c6819fc9d9db1
S.M. SADMAN ALAM AOPY <sadmanalamaopy@gmail.com>
2026-01-15 03:03:37 +0900

    Update DipuKhala.sol

--- DipuKhala.sol
+++ DipuKhala.sol
@@ -5,7 +5,7 @@ import {ERC20} from "@openzeppelin/contracts@5.5.0/token/ERC20/ERC20.sol";
 import {ERC20Permit} from "@openzeppelin/contracts@5.5.0/token/ERC20/extensions/ERC20Permit.sol";
 import {Ownable} from "@openzeppelin/contracts@5.5.0/access/Ownable.sol";
 
-/// @custom:security-contact contacttous@gmail.com
+
 contract DipuKhala is ERC20, Ownable, ERC20Permit {
     constructor(address recipient, address initialOwner)
         ERC20("DipuKhala", "DIPUKHALA")
@@ -19,4 +19,5 @@ contract DipuKhala is ERC20, Ownable, ERC20Permit {
         _mint(to, amount);
     }
 }
+
 
